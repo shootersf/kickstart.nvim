5a60300e2f15855776fbb4b0bf60f662a33c5ccb
Simon Farrelly <me@simonfarrelly.com>
2025-01-27 22:31:53 +0000

    keep visual highlight when shifting

--- init.lua
+++ init.lua
@@ -203,6 +203,10 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   end,
 })
 
+-- Simon yank override
+vim.keymap.set('v', '>', '>gv', { noremap = true, silent = true })
+vim.keymap.set('v', '<', '<gv', { noremap = true, silent = true })
+
 -- [[ Install `lazy.nvim` plugin manager ]]
 --    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
